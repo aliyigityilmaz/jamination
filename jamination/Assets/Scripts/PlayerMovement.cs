7c50d8fbb44a86b80215678149ee74e9bd0d779a
Ali Yiğit Yılmaz <redkom.yigit@gmail.com>
2024-05-10 23:11:03 +0300

    headbob

--- jamination/Assets/Scripts/PlayerMovement.cs
+++ jamination/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;
 
+    [SerializeField]
+    private bool canUseHeadBob = true;
+
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
     [SerializeField] private float gravity = 30.0f;
@@ -15,6 +18,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Head Bobbing Parameters")]
+    [SerializeField] private float walkBobSpeed = 14f;
+    [SerializeField] private float walkBobAmount = 0.05f;
+    private float defaultPosY = 0;
+    private float timer;
 
     private Camera playerCamera;
     private CharacterController controller;
@@ -28,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerCamera = GetComponentInChildren<Camera>();
         controller = GetComponent<CharacterController>();
+        defaultPosY = playerCamera.transform.localPosition.y;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -57,6 +67,7 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.y -= gravity * Time.deltaTime;
         }
         controller.Move(moveDirection * Time.deltaTime);
+        HandleHeadBob();
     }
 
     private void GetInput()
@@ -65,5 +76,17 @@ public class PlayerMovement : MonoBehaviour
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
         moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleHeadBob()
+    {
+        if (controller.isGrounded)
+        {
+            if (Mathf.Abs(moveDirection.x) > 0.1f || Mathf.Abs(moveDirection.z) > 0.1f)
+            {
+                timer += Time.deltaTime * walkBobSpeed;
+                playerCamera.transform.localPosition = new Vector3(playerCamera.transform.localPosition.x, defaultPosY + Mathf.Sin(timer) * walkBobAmount, playerCamera.transform.localPosition.z);
+            }
+        }
     }
 }

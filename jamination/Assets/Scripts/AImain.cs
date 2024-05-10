da4e0f9a16b445738076c3cc978b7388bb4df435
Umutkok <umutkok88@gmail.com>
2024-05-11 02:33:50 +0300

    1

---
+++ jamination/Assets/Scripts/AImain.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AImain : MonoBehaviour
+{
+    public Transform Following;
+    public static bool FollowCheck= true;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        if (FollowCheck && Following != null)
+        {
+            Vector3 dogruvektor = Following.position - transform.position;
+
+            Vector3 hýzvektor = dogruvektor.normalized * speed * Time.deltaTime;
+
+            transform.position += hýzvektor;
+        }
+    }
+
+
+
+
+}
